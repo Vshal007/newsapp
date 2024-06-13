b33d10716e37c298c9fe306f5a0285e9588cee95
Vshal007 <vshalpatel018@gmail.com>
2024-06-13 12:14:28 +0530

    Routing Added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 
 import './App.css';
 import React, { Component } from 'react'
-import Responsive from './components/Responsive';
-import Button from './components/Button';
-import Cards from './components/Cards';
-import Footer from './components/Footer';
-import Carousel from './components/Carousel';
-import DropdownHover from './components/DropdownHover';
-import DropdownBar from './components/DropdownBar';
-import Navbar from './components/Navbar/Navbar';
-import Crops from './components/Crops';
-import Services from './components/Services';
-import Activities from './components/Activities';
+import { Routes, Route, Router } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Homepage from './Pages/Homepage';
 
 
-let slides = [
-  "https://images.unsplash.com/photo-1484151709479-3996843263cf?h=500&w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
-  // "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
-  // "https://wallpapercave.com/wp/wp3386769.jpg",
-  // "https://wallpaperaccess.com/full/809523.jpg",
-  // "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
-  "https://images.unsplash.com/photo-1484151709479-3996843263cf?h=500&w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
-];
 export default class App extends Component {
 
 
@@ -29,17 +13,12 @@ export default class App extends Component {
 
     return (
       <div>
-
-        <Navbar />
-        <div className="w-[100%] z-0">
-          <Carousel slides={slides} />
-        </div>
-        <Services />
-        <Crops />
-        <Activities/> 
-
-        {/* <Cards /> */}
-        <Footer />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage/>} />
+          </Routes>
+        </BrowserRouter>
+        {/* <Cards /> */}     
         {/* <DropdownHover/>
          <DropdownBar/>
          <Carousel/>
